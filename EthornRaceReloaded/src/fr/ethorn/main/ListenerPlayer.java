0809c4dc7f7f632d215c7d96323f0671fd8ca85d
GODART <marin.godart@outlook.fr>
2017-07-03 21:47:02 +0200

    Implementation systeme de vie & loadData sql

--- EthornRaceReloaded/src/fr/ethorn/main/ListenerPlayer.java
+++ EthornRaceReloaded/src/fr/ethorn/main/ListenerPlayer.java
@@ -1,6 +1,8 @@
 package fr.ethorn.main;
 
+import com.mysql.fabric.xmlrpc.base.Array;
 import org.bukkit.Bukkit;
+import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,7 +19,6 @@ public class ListenerPlayer implements Listener {
      * Instance de la classe principale
      */
     private Main main;
-
     /**
      * Constructeur de la class
      * @param main Classe principale
@@ -37,10 +38,9 @@ public class ListenerPlayer implements Listener {
             PreparedStatement pS = main.SQL.getConnection().prepareStatement("SELECT COUNT(id) FROM user WHERE uuid = ?");
             pS.setString(1, uuid.toString());
             pS.execute();
-            ResultSet test =  pS.getResultSet();
-            if (test.next()){
-                int FirstConnection = test.getInt(1);
-
+            ResultSet datalogin =  pS.getResultSet();
+            if (datalogin.next()){
+                int FirstConnection = datalogin.getInt(1);
                 /**
                  * Si l'utilsateur ce connecte pour la premiere fois alors l'on l'ajoute a la bdd.
                  * Sinon On charge les données.
@@ -56,7 +56,85 @@ public class ListenerPlayer implements Listener {
                         e3.printStackTrace();
                     }
                 }else{
-                    //todo load dataplayer
+                    try {
+                        PreparedStatement ps3 = main.SQL.getConnection().prepareStatement("SELECT id_classe FROM user WHERE uuid=?");
+                        ps3.setString(1,uuid.toString());
+                        ps3.execute();
+                        ResultSet dataload =  ps3.getResultSet();
+                        dataload.next();
+
+                        Player p = e.getPlayer();
+                        /**
+                         * 1 = neophyte.
+                         * 2 = celeste --> séraphin
+                         * 3 = celeste --> déchu
+                         * 4 = maudite --> demon
+                         * 5 = maudite --> dragon
+                         * 6 = pure --> elfe
+                         * 7 = pure -->  fée
+                         * 8 = Tellurique --> Nain
+                         * 9 = Tellurique --> Gnome
+                         * 10 = Ethnique --> Humain
+                         * 11 = Ethnique --> Homme-bête
+                         * 12 = Divine -->  dieu unique
+                         * 13 = Divine --> dieu antique
+                         */
+                        switch (dataload.getObject(1).toString()){
+                            case "1":
+                                break;
+                            case "2":
+                                p.setHealthScale(p.getHealthScale() + 10);
+                                p.setMaxHealth(p.getMaxHealth() + 10D );
+                                break;
+                            case "3":
+                                p.setHealthScale(p.getHealthScale() + 6);
+                                p.setMaxHealth(p.getMaxHealth() + 6D );
+                                break;
+                            case "4":
+                                p.setHealthScale(p.getHealthScale() + 10);
+                                p.setMaxHealth(p.getMaxHealth() + 10D );
+                                break;
+                            case "5":
+                                p.setHealthScale(p.getHealthScale() + 20);
+                                p.setMaxHealth(p.getMaxHealth() + 20D );
+                                break;
+                            case "6":
+                                p.setHealthScale(p.getHealthScale() + 6);
+                                p.setMaxHealth(p.getMaxHealth() + 6D );
+                                break;
+                            case "7":
+                                p.setHealthScale(p.getHealthScale() - 4);
+                                p.setMaxHealth(p.getMaxHealth() - 4D );
+                                break;
+                            case "8":
+                                p.setHealthScale(p.getHealthScale() + -8);
+                                p.setMaxHealth(p.getMaxHealth() + -8D );
+                                break;
+                            case "9":
+                                p.setHealthScale(p.getHealthScale() + 6);
+                                p.setMaxHealth(p.getMaxHealth() + 6D );
+                                break;
+                            case "10":
+                                break;
+                            case "11":
+                                p.setHealthScale(p.getHealthScale() + 6);
+                                p.setMaxHealth(p.getMaxHealth() + 6D );
+                                break;
+                            case "12":
+                                p.setHealthScale(p.getHealthScale() + 40);
+                                p.setMaxHealth(p.getMaxHealth() + 40D );
+                                break;
+                            case "13":
+                                p.setHealthScale(p.getHealthScale() + 30);
+                                p.setMaxHealth(p.getMaxHealth() + 30D );
+
+                                break;
+                        }
+
+                        ps3.close();
+                    } catch (SQLException e3) {
+                        e3.printStackTrace();
+                    }
                 }
             }
             pS.close();
