5dae93cd7fdb439562e5bb2b417d24ce1be0281f
GODART <marin.godart@outlook.fr>
2017-07-05 16:26:46 +0200

    Fix bug des coeurs.

--- EthornRaceReloaded/src/fr/ethorn/main/ListenerPlayer.java
+++ EthornRaceReloaded/src/fr/ethorn/main/ListenerPlayer.java
@@ -79,50 +79,52 @@ public class ListenerPlayer implements Listener {
                             case "1":
                                 break;
                             case "2":
-                                p.setHealthScale(p.getHealthScale() + 10);
-                                p.setMaxHealth(p.getMaxHealth() + 10D );
+                                p.setHealthScale(30);
+                                p.setMaxHealth(30D);
                                 break;
                             case "3":
-                                p.setHealthScale(p.getHealthScale() + 6);
-                                p.setMaxHealth(p.getMaxHealth() + 6D );
+                                p.setHealthScale(26);
+                                p.setMaxHealth(26D );
                                 break;
                             case "4":
-                                p.setHealthScale(p.getHealthScale() + 10);
-                                p.setMaxHealth(p.getMaxHealth() + 10D );
+                                p.setHealthScale(30);
+                                p.setMaxHealth(30D);
                                 break;
                             case "5":
-                                p.setHealthScale(p.getHealthScale() + 20);
-                                p.setMaxHealth(p.getMaxHealth() + 20D );
+                                p.setHealthScale(40);
+                                p.setMaxHealth(40D);
                                 break;
                             case "6":
-                                p.setHealthScale(p.getHealthScale() + 6);
-                                p.setMaxHealth(p.getMaxHealth() + 6D );
+                                p.setHealthScale(26);
+                                p.setMaxHealth(26D);
                                 break;
                             case "7":
-                                p.setHealthScale(p.getHealthScale() - 4);
-                                p.setMaxHealth(p.getMaxHealth() - 4D );
+                                p.setHealthScale(16);
+                                p.setMaxHealth(16D);
                                 break;
                             case "8":
-                                p.setHealthScale(p.getHealthScale() + -8);
-                                p.setMaxHealth(p.getMaxHealth() + -8D );
+                                p.setHealthScale(12);
+                                p.setMaxHealth(12D);
                                 break;
                             case "9":
-                                p.setHealthScale(p.getHealthScale() + 6);
-                                p.setMaxHealth(p.getMaxHealth() + 6D );
+                                p.setHealthScale(26);
+                                p.setMaxHealth(26D );
                                 break;
                             case "10":
                                 break;
                             case "11":
-                                p.setHealthScale(p.getHealthScale() + 6);
-                                p.setMaxHealth(p.getMaxHealth() + 6D );
+                                p.setHealthScale(26);
+                                p.setMaxHealth(26D );
                                 break;
                             case "12":
-                                p.setHealthScale(p.getHealthScale() + 40);
-                                p.setMaxHealth(p.getMaxHealth() + 40D );
+                                p.setHealthScale(60);
+                                p.setMaxHealth(60D);
                                 break;
                             case "13":
-                                p.setHealthScale(p.getHealthScale() + 30);
-                                p.setMaxHealth(p.getMaxHealth() + 30D );
+                                //todo  p.setStatistic(Statistic.DAMAGE_DEALT,p.getStatistic(Statistic.DEATHS) + 2);
+
+                                p.setHealthScale(50);
+                                p.setMaxHealth(50D);
 
                                 break;
                         }
