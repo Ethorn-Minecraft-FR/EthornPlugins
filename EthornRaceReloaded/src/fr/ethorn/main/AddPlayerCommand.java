6a16af463b0241446028afd6899793c8f9f6e05b
GODART <marin.godart@outlook.fr>
2017-07-06 18:20:54 +0200

    Optimisation de la commande

--- EthornRaceReloaded/src/fr/ethorn/main/AddPlayerCommand.java
+++ EthornRaceReloaded/src/fr/ethorn/main/AddPlayerCommand.java
@@ -11,28 +11,20 @@ import java.util.Objects;
 public class AddPlayerCommand implements CommandExecutor{
 
 
-
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if (command.getName().equalsIgnoreCase("ethorn")){
-            if(Objects.equals(strings[0], "player")){
-                if(strings[1] != null){
-                    if(Objects.equals(strings[2], "add")){
-                        if(strings[3] != null ){
-                            String playerName = strings[1];
-                            String rank = strings[3];
-                            try {
-                                PreparedStatement pS4 = Main.instance.SQL.getConnection().prepareStatement("UPDATE `user` SET `id_classe`= ? WHERE (`playername`= ? )");
-                                pS4.setString(1, rank);
-                                pS4.setString(2, playerName);
-                                pS4.execute();
-                                pS4.close();
-                            } catch (SQLException e3) {
-                                e3.printStackTrace();
-                            }
-
-                        }
-                    }
+            if(Objects.equals(strings[0], "player") && strings[1] != null && Objects.equals(strings[2], "add") && strings[3] != null){
+                String playerName = strings[1];
+                String rank = strings[3];
+                try {
+                    PreparedStatement pS4 = Main.instance.SQL.getConnection().prepareStatement("UPDATE `user` SET `id_classe`= ? WHERE (`playername`= ? )");
+                    pS4.setString(1, rank);
+                    pS4.setString(2, playerName);
+                    pS4.execute();
+                    pS4.close();
+                } catch (SQLException e3) {
+                    e3.printStackTrace();
                 }
             }
         }
