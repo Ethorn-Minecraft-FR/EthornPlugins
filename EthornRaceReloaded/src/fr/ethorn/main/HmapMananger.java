c510cc081729c45f3e9258edf623a5f035248c97
GODART <marin.godart@outlook.fr>
2017-07-07 22:22:49 +0200

    Ajout d'un HmapManager
    AjoutHmap &&  ValueExistHmap on été mis mais ne sont pas implémenter

---
+++ EthornRaceReloaded/src/fr/ethorn/main/HmapMananger.java
@@ -0,0 +1,31 @@
+package fr.ethorn.main;
+
+import java.util.UUID;
+
+/**
+ * Created by marin on 07/07/2017.
+ */
+public class HmapMananger {
+
+    /**
+     * @param uuid The uuid of the player to add in the hmap
+     * @param cast The cast int value.
+     *
+     * Permet d'ajouter un joueur dans la hmap ce qui permet de faire un traitement de donnée par la suite.
+     */
+    public static void AjoutHmap(UUID uuid,int cast){
+
+    }
+
+    /**
+     * @param uuid The uuid of the player to add in the hmap
+     * @param cast The cast int value.
+     * @return booleen true if value exist in hmap, false if value doesnt exist
+     * Permet de savoir si la valeur est existante dans la hmap.
+     */
+    public static boolean ValueExistHmap(UUID uuid,int cast){
+        boolean success = false;
+
+        return success;
+    }
+}
