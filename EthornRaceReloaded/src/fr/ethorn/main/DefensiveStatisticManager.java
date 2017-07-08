285a445e4b31bb5b39fb2764b79c533bbb58dc6c
GODART <marin.godart@outlook.fr>
2017-07-08 21:22:55 +0200

    Implementation du DefensiveStatisticManager avec fake value

---
+++ EthornRaceReloaded/src/fr/ethorn/main/DefensiveStatisticManager.java
@@ -0,0 +1,58 @@
+package fr.ethorn.main;
+
+/**
+ * Created by marin on 08/07/2017.
+ */
+public class DefensiveStatisticManager {
+
+    public static int getDefenseStats(int castId){
+        int defensevalue = 0;
+
+        switch (castId) {
+            case 1:
+                defensevalue = 0;
+                break;
+            case 2:
+                defensevalue = 0;
+                break;
+            case 3:
+                defensevalue = 0;
+                break;
+            case 4:
+                defensevalue = 0;
+                break;
+            case 5:
+                defensevalue = 0;
+                break;
+            case 6:
+                defensevalue = 0;
+                break;
+            case 7:
+                defensevalue = 0;
+                break;
+            case 8:
+                defensevalue = 0;
+                break;
+            case 9:
+                defensevalue = 0;
+                break;
+            case 10:
+                defensevalue = 0;
+                break;
+            case 11:
+                defensevalue = 0;
+                break;
+            case 12:
+                defensevalue = 0;
+                break;
+            case 13:
+                defensevalue = 0;
+                break;
+            default:
+                break;
+        }
+
+
+        return defensevalue;
+    }
+}
